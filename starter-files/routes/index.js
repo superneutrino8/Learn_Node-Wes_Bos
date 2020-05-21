e5ba2db93b6d795b97d01bc5deb2148bc0badcf7
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-05-21 11:34:24 +0530

    looking into routes and pug

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,9 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get("/", (req, res) => {
+  res.send("Hey! It works!");
+});
+
+router.get("/home/", (req, res) => {
+  res.render("home", {
+    name: req.query.name,
+  });
 });
 
 module.exports = router;
