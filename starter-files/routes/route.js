1773b984a6359ddb0931a195d504677c746956db
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-05-22 11:48:14 +0530

    made changes in route file

--- starter-files/routes/route.js
+++ starter-files/routes/route.js
@@ -7,6 +7,9 @@ router.get("/", (req, res) => {
   res.send("Hey! It works!");
 });
 
+console.log("Dir: " + __dirname);
+console.log("File: " + __filename);
+
 router.get("/home/", storeController.homePage);
 router.get("/add/", storeController.addStorePage);
 router.post("/add/", storeController.createStore);
