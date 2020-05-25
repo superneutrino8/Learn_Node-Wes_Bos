dc7c2a9e0d97c808bfeb5e6bf1d6d9a048d7ac1f
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-05-25 15:16:50 +0530

    Error Handle

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require("mongoose");
+const Store = mongoose.model("Store");
+
 exports.homePage = (req, res) => {
     res.render("home", {
         name: "Wes",
@@ -12,6 +15,8 @@ exports.addStorePage = (req, res) => {
     });
 };
 
-exports.createStore = (req, res) => {
-    res.json(req.body);
+exports.createStore = async (req, res) => {
+    const store = new Store(req.body);
+    await store.save();
+    console.log("It Works");
 };
--- starter-files/routes/route.js
+++ starter-files/routes/route.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const storeController = require("../controllers/storeController");
 
+const { catchErrors } = require("../handlers/errorHandlers");
+
 // Do work here
 router.get("/", (req, res) => {
     res.send("Hey! It works!");
@@ -12,6 +14,6 @@ router.get("/", (req, res) => {
 
 router.get("/home/", storeController.homePage);
 router.get("/add/", storeController.addStorePage);
-router.post("/add/", storeController.createStore);
+router.post("/add/", catchErrors(storeController.createStore));
 
 module.exports = router;
