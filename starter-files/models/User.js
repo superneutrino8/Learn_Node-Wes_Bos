ac13f86669705995dad23f0ac2d18c30bdbe2950
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-06-08 16:47:19 +0530

    Working on User Model

---
+++ starter-files/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+
+const md5 = require("md5");
+const validator = require("validator");
+const mongodbErrorHandler = require("mongoose-mongodb-errors");
+const passpoerLocalMongoose = require("passport-local-mongoose");
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate: [validator.isEmail, "Invalid Email Address"],
+        required: "Please supply an Email Address",
+    },
+    name: {
+        type: String,
+        required: "Please supply a name",
+        trim: true,
+    },
+});
+
+module.exports = mongoose.model("User", userSchema);
