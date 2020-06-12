2c1180b043cb21de59b62be2f5a120a9a942de43
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-06-12 16:08:21 +0530

    automplete working now

--- starter-files/public/javascripts/modules/typeAhead.js
+++ starter-files/public/javascripts/modules/typeAhead.js
@@ -1,4 +1,5 @@
-const axios = require("axios");
+import axios from "axios";
+import dompurify from "dompurify";
 
 function searchResultHTML(stores) {
     return stores
@@ -22,13 +23,49 @@ function typeAhead(search) {
             return;
         }
         searchResult.style.display = "block";
-        axios.get(`/api/search/?q=${this.value}`).then((res) => {
-            if (res.data.length) {
-                searchResult.innerHTML = searchResultHTML(res.data);
-                return;
-            }
-            searchResult.innerHTML = `<div class="search__result">No results for ${this.value}</div>`;
-        });
+        axios
+            .get(`/api/search/?q=${this.value}`)
+            .then((res) => {
+                if (res.data.length) {
+                    searchResult.innerHTML = dompurify.sanitize(
+                        searchResultHTML(res.data)
+                    );
+                    return;
+                }
+                searchResult.innerHTML = dompurify.sanitize(
+                    `<div class="search__result">No results for ${this.value}</div>`
+                );
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    });
+
+    searchInput.on("keyup", function(e) {
+        if (![38, 40, 13].includes(e.keyCode)) {
+            return;
+        }
+        const activeClass = "search__result--active";
+        const current = search.querySelector(`.${activeClass}`);
+        const items = search.querySelectorAll(".search__result");
+        let next;
+        if (e.keyCode === 40 && current) {
+            next = current.nextElementSibling || items[0];
+        } else if (e.keyCode === 40) {
+            next = items[0];
+        } else if (e.keyCode === 38 && current) {
+            next = current.previousElementSibling || items[items.length - 1];
+        } else if (e.keyCode === 38) {
+            next = items[items.length - 1];
+        } else if (e.keyCode === 13 && current.href) {
+            window.location = current.href;
+            return;
+        }
+        console.log("Current: ", current);
+        if (current) {
+            current.classList.remove(activeClass);
+        }
+        next.classList.add(activeClass);
     });
 }
 
