cb7d5860474c19fd256b2030e3b53c5659de8742
Vivek Bariya <vivekbaraiya7@gmail.com>
2020-07-02 23:45:52 +0530

    added heart module to profile

--- starter-files/public/dist/App.bundle.js
+++ starter-files/public/dist/App.bundle.js
@@ -3415,6 +3415,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _modules_autocomplete__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./modules/autocomplete */ "./public/javascripts/modules/autocomplete.js");
 /* harmony import */ var _modules_typeAhead__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./modules/typeAhead */ "./public/javascripts/modules/typeAhead.js");
 /* harmony import */ var _modules_map__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./modules/map */ "./public/javascripts/modules/map.js");
+/* harmony import */ var _modules_heart__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./modules/heart */ "./public/javascripts/modules/heart.js");
+
 
 
 
@@ -3423,6 +3425,8 @@ __webpack_require__.r(__webpack_exports__);
 Object(_modules_autocomplete__WEBPACK_IMPORTED_MODULE_2__["default"])(Object(_modules_bling__WEBPACK_IMPORTED_MODULE_1__["$"])("#address"), Object(_modules_bling__WEBPACK_IMPORTED_MODULE_1__["$"])("#lat"), Object(_modules_bling__WEBPACK_IMPORTED_MODULE_1__["$"])("#lng"));
 Object(_modules_typeAhead__WEBPACK_IMPORTED_MODULE_3__["default"])(Object(_modules_bling__WEBPACK_IMPORTED_MODULE_1__["$"])(".search"));
 Object(_modules_map__WEBPACK_IMPORTED_MODULE_4__["default"])(Object(_modules_bling__WEBPACK_IMPORTED_MODULE_1__["$"])("#map"));
+var heartForms = Object(_modules_bling__WEBPACK_IMPORTED_MODULE_1__["$$"])("form.heart");
+heartForms.on("submit", _modules_heart__WEBPACK_IMPORTED_MODULE_5__["default"]);
 
 /***/ }),
 
@@ -3481,6 +3485,44 @@ NodeList.prototype.on = NodeList.prototype.addEventListener = function (name, fn
 };
 
 
+
+/***/ }),
+
+/***/ "./public/javascripts/modules/heart.js":
+/*!*********************************************!*\
+  !*** ./public/javascripts/modules/heart.js ***!
+  \*********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _bling__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./bling */ "./public/javascripts/modules/bling.js");
+
+
+
+function ajaxHeart(e) {
+  var _this = this;
+
+  e.preventDefault();
+  axios__WEBPACK_IMPORTED_MODULE_0___default.a.post(this.action).then(function (res) {
+    var isHearted = _this.heart.classList.toggle("heart__button--hearted");
+
+    Object(_bling__WEBPACK_IMPORTED_MODULE_1__["$"])(".heart-count").textContent = res.data.hearts.length;
+
+    if (isHearted) {
+      _this.heart.classList.add("heart__button--float");
+
+      setTimeout(function () {
+        return _this.heart.classList.remove("heart__button--float");
+      }, 2500);
+    }
+  })["catch"](console.error);
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (ajaxHeart);
 
 /***/ }),
 
